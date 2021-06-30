54a98491c5c62fe1bb2e9345ba18bc9280695c16
GennadiySerikov <serikov.gen@yandex.ru>
2021-06-30 15:37:39 +0300

    create empty solution

---
+++ intro1__hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intro1__hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
